b913637953a876ef748731d1558ebf935cffaa48
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-05-20 09:00:40 -0500

    add app and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
-export default function App() {
-    return (
-        <div>
-            hey from app
-        </div>
-    )
+function App() {
+  const [value, setValue] = useState('')
+  const [name, setName] = useState('')
+  const [touched, setTouched] = useState(false)
+  const [valid, setValid] = useState(false)
+
+  useEffect(() => {
+    setValid(value.length <= 18)
+  }, [value]);
+
+  return (
+    <div className="container">
+
+      <form onSubmit={e => {
+        e.preventDefault()
+        const isValid = touched && valid
+        if (isValid) { 
+           setName(value) 
+        }
+        }}>
+
+        <input type="text" value={value} onChange={e => {
+            setTouched(true)
+            setValue(e.target.value)
+          }
+        }/>
+
+     </form>
+     <p>My name is : {value}</p>
+     <p>Your name is : {name}</p>
+
+     <p className={`small ${!valid? 'highlight': ''}`}>Max of 18 characters</p>
+    </div>  
+  );
 }
+
+export default App;
